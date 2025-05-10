3ce763c241e41c508b0ff29ecad315456b56838b
Camilo <juan.cristancho-v@mail.escuelaing.edu.co>
2025-05-10 18:06:48 -0500

    presentacion hasta menu de opcines

--- POOBKEMON/src/presentation/PoobkemonGUI.java
+++ POOBKEMON/src/presentation/PoobkemonGUI.java
@@ -1,11 +1,13 @@
 package presentation;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
+import java.util.Hashtable;
+
 
 public class PoobkemonGUI extends JFrame {
     private JLabel logoPoobkemon;
@@ -15,6 +17,13 @@ public class PoobkemonGUI extends JFrame {
     private JButton butonJugadorVsJugador;
     private JButton butonJugadorVsMaquina;
     private JButton butonMaquinaVsMaquina;
+    private JButton butonAttackingTrainer;
+    private JButton butonDefensiveTrainer;
+    private JButton butonChangingTrainer;
+    private JButton butonExpertTrainer;
+    private JButton butonContinue;
+    private JButton butonBack;
+
 
     PoobkemonGUI(){
         prepareElements();
@@ -56,6 +65,7 @@ public class PoobkemonGUI extends JFrame {
      */
     private void animacionInicial() {
         menuInicial();
+        accionesMenuIniciales();
     }
 
 
@@ -93,14 +103,13 @@ public class PoobkemonGUI extends JFrame {
         gbc.weighty = 0.3;
         gbc.weightx = 1.0;
 
-        Image start = new ImageIcon(getClass().getResource("/presentation/recursos/pressstart.gif")).getImage();
-        Image startEscalada = start.getScaledInstance(getWidth()/4, 75, Image.SCALE_SMOOTH);
+        ImageIcon start = new ImageIcon(getClass().getResource("/presentation/recursos/pressstart.gif"));
 
-        butonPressStart = new JButton(new ImageIcon(startEscalada));
+        butonPressStart = new JButton(start);
         botonComoImagen(butonPressStart);
         add(butonPressStart, gbc);
 
-        // Panel 3 - Ocupa 2/10 de la altura
+        // Panel 3
         gbc.gridy = 2;
         gbc.weighty = 0.2;
         gbc.weightx = 1.0;
@@ -109,11 +118,10 @@ public class PoobkemonGUI extends JFrame {
 
         setVisible(true);
     }
-    
+
     private void accionesMenuIniciales(){
         butonPressStart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                limpiarJFrame();
                 menuDeOpciones();
                 accionesMenuDeOpciones();
             }
@@ -153,21 +161,23 @@ public class PoobkemonGUI extends JFrame {
     private void accionesMenuDeOpciones(){
         butonIniciarPartida.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                limpiarJFrame();
+
                 menuDeSeleccion();
+                accionesmenuDeSeleccion();
 
             }
         });
 
         butonOpciones.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                limpiarJFrame();
+
             }
         });
 
     }
 
     private void menuDeSeleccion(){
+
         ImageIcon fondo = new ImageIcon(getClass().getResource("/presentation/recursos/fondo1.png"));
         JPanel fondoFinal = new JPanel() {
             @Override
@@ -182,101 +192,178 @@ public class PoobkemonGUI extends JFrame {
         //LAYOUT PRINCIPAL
         setLayout(new GridLayout(2,0));
 
-        // LAYOUT SECUNDARIO 1
-        JPanel panel1 = new JPanel(new BorderLayout()); // Espaciado horizontal y vertical de 10px
+        // LAYOUT SUPERIOR (parte de arriba)
+        JPanel panelSuperior = new JPanel(new BorderLayout());
+        panelSuperior.setOpaque(false);
 
-        // Crear los paneles para cada región con colores distintos
+        // PARTE SUPERIOR IZQUIERDA (zona de texto y gif Pokemon)
+        JPanel superiorIzquierda = new JPanel(new BorderLayout());
+        superiorIzquierda.setOpaque(false);
 
-        // Panel NORTE (arriba)
-        JPanel panelNorte1 = crearPanelConEtiqueta("NORTE (North)", Color.RED);
-        panel1.add(panelNorte1, BorderLayout.NORTH);
+        JLabel textoSeleccion = new JLabel(imagenEscalada("/presentation/recursos/texto1.png",1,4,1,6));
+        textoSeleccion.setHorizontalAlignment(SwingConstants.CENTER);
+        superiorIzquierda.add(textoSeleccion,BorderLayout.NORTH);
 
-        // Panel SUR (abajo)
-        JPanel panelSur1 = crearPanelConEtiqueta("SUR (South)", Color.BLUE);
-        panel1.add(panelSur1, BorderLayout.SOUTH);
+        JLabel imagenApoyo = new JLabel(new ImageIcon(getClass().getResource("/presentation/recursos/snorlax.gif")));
+        imagenApoyo.setHorizontalAlignment(SwingConstants.CENTER);
+        imagenApoyo.setOpaque(false);
+        superiorIzquierda.add(imagenApoyo,BorderLayout.CENTER);
 
-        // Panel ESTE (derecha)
-        JPanel panelEste1 = crearPanelConEtiqueta("ESTE (East)", Color.GREEN);
-        panel1.add(panelEste1, BorderLayout.EAST);
+        textoSeleccion.setPreferredSize(new Dimension(0, getWidth()/8));
+        superiorIzquierda.setPreferredSize(new Dimension(getWidth()*3/10, 0));
 
-        // Panel OESTE (izquierda)
-        JPanel panelOeste1 = crearPanelConEtiqueta("OESTE (West)", Color.YELLOW);
-        panel1.add(panelOeste1, BorderLayout.WEST);
 
-        // Panel CENTRO (centro)
-        JPanel panelCentro1 = crearPanelConEtiqueta("CENTRO (Center)", Color.ORANGE);
-        panel1.add(panelCentro1, BorderLayout.CENTER);
+        // PANEL SUPERIOR DERECHA (modos de juego)
+        JPanel superiorDerecha = new JPanel(new GridLayout(3,0));
+        superiorDerecha.setOpaque(false);
+
+        butonJugadorVsJugador = new JButton(imagenEscalada("/presentation/recursos/jugadorVsjugador.png",2,3,1,7));
+        botonComoImagen(butonJugadorVsJugador);
+        butonJugadorVsMaquina = new JButton(imagenEscalada("/presentation/recursos/jugadorVsmaquina.png",2,3,1,7));
+        botonComoImagen(butonJugadorVsMaquina);
+        butonMaquinaVsMaquina = new JButton(imagenEscalada("/presentation/recursos/maquinaVsmaquina.png",2,3,1,7));
+        botonComoImagen(butonMaquinaVsMaquina);
+
+        superiorDerecha.add(butonJugadorVsJugador);
+        superiorDerecha.add(butonJugadorVsMaquina);
+        superiorDerecha.add(butonMaquinaVsMaquina);
+
+
+        panelSuperior.add(superiorDerecha, BorderLayout.CENTER);
+        panelSuperior.add(superiorIzquierda, BorderLayout.WEST);
+
+
+        // LAYOUT INFERIOR
+        JPanel panelInferior = new JPanel(new BorderLayout());
+        panelInferior.setOpaque(false);
+
+        // PARTE INFERIOR IZQUIERDA (modos de maquina)
+        JPanel inferiorIzquierda = new JPanel(new BorderLayout());
+        inferiorIzquierda.setOpaque(false);
+
+        //BORDER LAYOUT PARTE DE LOS BOTONES
+        JLabel subNorte = new JLabel("Selecciona La dificultad de la maquina");
+        subNorte.setHorizontalAlignment(SwingConstants.CENTER);
+        subNorte.setOpaque(false);
+
+        subNorte.setPreferredSize(new Dimension(0, getHeight()/20));
+        inferiorIzquierda.add(subNorte,BorderLayout.NORTH);
+
+        JPanel subCenter = new JPanel(new GridLayout(2,2));
+        subCenter.setOpaque(false);
+
+        butonDefensiveTrainer = new JButton(imagenEscalada("/presentation/recursos/defensiveTrainer.png",1,6,1,6));
+        botonComoImagen(butonDefensiveTrainer);
+        butonAttackingTrainer = new JButton(imagenEscalada("/presentation/recursos/attackingTrainer.png",1,6,1,6));
+        botonComoImagen(butonAttackingTrainer);
+        butonChangingTrainer = new JButton(imagenEscalada("/presentation/recursos/changingTrainer.png",1,6,1,6));
+        botonComoImagen(butonChangingTrainer);
+        butonExpertTrainer= new JButton(imagenEscalada("/presentation/recursos/expertTrainer.png",1,6,1,6));
+        botonComoImagen(butonExpertTrainer);
+
+        subCenter.add(butonDefensiveTrainer);
+        subCenter.add(butonAttackingTrainer);
+        subCenter.add(butonChangingTrainer);
+        subCenter.add(butonExpertTrainer);
+
+        inferiorIzquierda.add(subCenter,BorderLayout.CENTER);
+
+        JPanel subWest = new JPanel();
+        subWest.setOpaque(false);
+        subWest.setPreferredSize(new Dimension(getWidth()/20, 0));
+        inferiorIzquierda.add(subWest,BorderLayout.EAST);
+
+        JPanel subEast = new JPanel();
+        subEast.setOpaque(false);
+        subEast.setPreferredSize(new Dimension(getWidth()/20,0 ));
+        inferiorIzquierda.add(subEast,BorderLayout.WEST);
+
+        JPanel subSouth = new JPanel();
+        subSouth.setOpaque(false);
+        subSouth.setPreferredSize(new Dimension(0, getHeight()/20));
+        inferiorIzquierda.add(subSouth,BorderLayout.SOUTH);
+
+        panelInferior.add(inferiorIzquierda, BorderLayout.WEST);
+
+        // INFERIOR DERECHA (modo de juego y botones de continuar y atras)
+        JPanel inferiorDerecha = new JPanel(new GridLayout(2,0));
+        inferiorDerecha.setOpaque(false);
+
+        // slider para modo de juego
+        JPanel sliderModo = new JPanel(new BorderLayout());
+        sliderModo.setOpaque(false);
+
+        JPanel subWestInf = new JPanel();
+        subWestInf.setOpaque(false);
+        subWestInf.setPreferredSize(new Dimension(getWidth()/20, 0));
+        sliderModo.add(subWestInf,BorderLayout.EAST);
+
+        JPanel subEastInf = new JPanel();
+        subEastInf.setOpaque(false);
+        subEastInf.setPreferredSize(new Dimension(getWidth()/20,0 ));
+        sliderModo.add(subEastInf,BorderLayout.WEST);
+
+        JLabel subNorthtInf = new JLabel("MODO DE JUEGO");
+        subNorthtInf.setHorizontalAlignment(SwingConstants.CENTER);
+        subNorthtInf.setOpaque(false);
+        subNorthtInf.setPreferredSize(new Dimension(0,getWidth()/20 ));
+        sliderModo.add(subNorthtInf,BorderLayout.NORTH);
+
+
+        JSlider seleccionarModo = new JSlider(0,1,0);
+        seleccionarModo.setOpaque(false);
+
+        seleccionarModo.setMajorTickSpacing(1);
+        seleccionarModo.setPaintTicks(true);
+        seleccionarModo.setSnapToTicks(true);
+        seleccionarModo.setPaintLabels(true);
+
+        Hashtable<Integer, JLabel> labelTable = new Hashtable<>();
+        labelTable.put(0, new JLabel("Normal"));
+        labelTable.put(1, new JLabel("Survival"));
+        seleccionarModo.setLabelTable(labelTable);
+
+        sliderModo.add(seleccionarModo,BorderLayout.CENTER);
+
+        inferiorDerecha.add(sliderModo);
+
+        JPanel opcionesFinales = new JPanel(new GridLayout(0,2));
+        opcionesFinales.setOpaque(false);
+
+        butonContinue = new JButton(imagenEscalada("/presentation/recursos/continuar.png",2,9,2,9));
+        botonComoImagen(butonContinue);
+        butonBack = new JButton(imagenEscalada("/presentation/recursos/volver.png",2,9,2,9));
+        botonComoImagen(butonBack);
+
+        opcionesFinales.add(butonContinue);
+        opcionesFinales.add(butonBack);
+
+        inferiorDerecha.add(opcionesFinales);
+
+        panelInferior.add(inferiorDerecha, BorderLayout.CENTER);
+
 
         // Establecer dimensiones preferidas para controlar tamaños iniciales
-        panelNorte1.setPreferredSize(new Dimension(0, 0)); // Alto de 100px
-        panelSur1.setPreferredSize(new Dimension(0, 0)); // Alto de 100px
-        panelEste1.setPreferredSize(new Dimension(0, 0)); // Ancho de 150px
-        panelOeste1.setPreferredSize(new Dimension(getWidth()*3/10, 0)); // Ancho de 150px
+        inferiorIzquierda.setPreferredSize(new Dimension(getWidth()/2, 0)); // Ancho de 150px
 
-
-        // LAYOUT SECUNDARIO 2
-        JPanel panel2 = new JPanel(new BorderLayout()); // Espaciado horizontal y vertical de 10px
-        panel2.setBackground(Color.WHITE);
-
-        // Crear los paneles para cada región con colores distintos
-
-        // Panel NORTE (arriba)
-        JPanel panelNorte2 = crearPanelConEtiqueta("NORTE (North)", Color.RED);
-        panel2.add(panelNorte2, BorderLayout.NORTH);
-
-        // Panel SUR (abajo)
-        JPanel panelSur2 = crearPanelConEtiqueta("SUR (South)", Color.BLUE);
-        panel2.add(panelSur2, BorderLayout.SOUTH);
-
-        // Panel ESTE (derecha)
-        JPanel panelEste2 = crearPanelConEtiqueta("ESTE (East)", Color.GREEN);
-        panel2.add(panelEste2, BorderLayout.EAST);
-
-        // Panel OESTE (izquierda)
-        JPanel panelOeste2 = crearPanelConEtiqueta("OESTE (West)", Color.YELLOW);
-        panel2.add(panelOeste2, BorderLayout.WEST);
-
-        // Panel CENTRO (centro)
-        JPanel panelCentro2 = crearPanelConEtiqueta("CENTRO (Center)", Color.ORANGE);
-        panel2.add(panelCentro2, BorderLayout.CENTER);
-
-        // Establecer dimensiones preferidas para controlar tamaños iniciales
-        panelNorte2.setPreferredSize(new Dimension(0, 0)); // Alto de 100px
-        panelSur2.setPreferredSize(new Dimension(0, 0)); // Alto de 100px
-        panelEste2.setPreferredSize(new Dimension(0, 0)); // Ancho de 150px
-        panelOeste2.setPreferredSize(new Dimension(getWidth()/2, 0)); // Ancho de 150px
-
-        add(panel1);
-        add(panel2);
+        add(panelSuperior);
+        add(panelInferior);
         setLocationRelativeTo(null);
         setVisible(true);
 
-
-        Image jugadorVsjugador = new ImageIcon(getClass().getResource("/presentation/recursos/jugadorVsjugador.png")).getImage();
-        Image jugadorVsjugadorEscalada = jugadorVsjugador.getScaledInstance(getWidth()*3/5, getHeight()/8, Image.SCALE_SMOOTH);
-        butonJugadorVsJugador = new JButton(new ImageIcon(jugadorVsjugadorEscalada));
         botonComoImagen(butonJugadorVsJugador);
-
-
-
-
-
 
         setVisible(true);
     }
-    private JPanel crearPanelConEtiqueta(String texto, Color color) {
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.setBackground(color);
-        panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-
-        JLabel label = new JLabel(texto);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 18));
-        label.setForeground(Color.WHITE);
-        panel.add(label, BorderLayout.CENTER);
-
-        return panel;
-    }
+    private void accionesmenuDeSeleccion(){
+        butonContinue.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                limpiarJFrame();
+                PokedexGUI pokedex = new PokedexGUI();
+                add(pokedex);
+            }
+        });
+    };
 
 
     /**
@@ -284,7 +371,7 @@ public class PoobkemonGUI extends JFrame {
      * @param b referenciando al boton
      */
     private void botonComoImagen(JButton b) {
-        b.setBorderPainted(false);
+
         b.setContentAreaFilled(false);
         b.setFocusPainted(false);
     }
@@ -305,7 +392,24 @@ public class PoobkemonGUI extends JFrame {
             System.exit(0);
         }
     }
-    
+
+    /**
+     *
+     * @param ruta
+     * @param anchoN
+     * @param anchoD
+     * @param altoN
+     * @param altoD
+     * @return
+     */
+    private ImageIcon imagenEscalada(String ruta,int anchoN,int anchoD,int altoN,int altoD){
+
+
+        Image jugadorVsjugador = new ImageIcon(getClass().getResource(ruta)).getImage();
+        Image jugadorVsjugadorEscalada = jugadorVsjugador.getScaledInstance((getWidth()*anchoN)/anchoD, (getHeight()*altoN)/altoD, Image.SCALE_SMOOTH);
+        return new ImageIcon(jugadorVsjugadorEscalada);
+    }
+
     /**
      * Limpia completamente un JFrame: remueve todos los componentes
      *
